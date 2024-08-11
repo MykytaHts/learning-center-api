fbdc40f1dbeb30002046484eff61d5e1379bc267
mykyta.hotsii <holeghalat@gmail.com>
2024-08-11 13:36:38 +0300

    Bucket connection testing.

--- src/main/java/com/example/learningcenterapi/configuration/GoogleStorageConfig.java
+++ src/main/java/com/example/learningcenterapi/configuration/GoogleStorageConfig.java
@@ -1,20 +1,28 @@
 package com.example.learningcenterapi.configuration;
 
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 public class GoogleStorageConfig {
 
-//    @Bean
-//    public Bucket getHomeworkBucket(final Storage storage) {
-//        return null;
-//    }
-//
-//
-//    @Bean
-//    public Storage getStorage() {
-//        return null;
-//    }
+
+    private final String homeworkBucketName = "bucketerria";
+
+    @Bean
+    public Bucket getHomeworkBucket(final Storage storage) {
+        return storage.get(homeworkBucketName, Storage.BucketGetOption.fields());
+    }
+
+    @Bean
+    public Storage getStorage() {
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        log.info("Connected to Google Cloud using Application Default Credentials");
+        return storage;
+    }
 }
